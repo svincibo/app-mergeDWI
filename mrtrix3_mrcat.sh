85f26cc9436204d8c19fcdb59d53651c76d1ddc2
Sophia Vinci-Booher <svincibo@gmail.com>
2020-02-10 16:20:13 -0500

    made app more generalizable

--- mrtrix3_mrcat.sh
+++ mrtrix3_mrcat.sh
@@ -12,31 +12,39 @@ set -x
 set -e
 
 # Parse input file names from config.json. 
-dwi=$(jq -r .dwis config.json.sample)
-bvals=$(jq -r .bvals config.json.sample)
-bvecs=$(jq -r .bvecs config.json.sample)
+dwi=($(jq -r .dwis[] config.json))
+bvals=($(jq -r .bvals[] config.json))
+bvecs=($(jq -r .bvecs[] config.json))
 
 # Inform user.
 echo "Converting input files to mrtrix format..."
 
 # Convert nifti to mif for use with mritrix3.
-mrconvert -fslgrad $bvecs1 $bvals1 $dwi1 raw1.mif --export_grad_mrtrix raw1.b -quiet -force
-mrconvert -fslgrad $bvecs2 $bvals2 $dwi2 raw2.mif --export_grad_mrtrix raw2.b -quiet -force
+inputs=[];
+for d in ${!dwi[@]}; do
+
+	mrconvert -fslgrad ${bvecs[$d]} ${bvals[$d]} ${dwi[$d]} raw${d}.mif --export_grad_mrtrix raw${d}.b -quiet -force
+
+done
 
 # Inform user.
 echo "Merging dwi images..."
 
-# Concatenate dwi1 and dwi2 using mrcat.
-mrcat raw1.mif raw2.mif dwi_out.mif
+# Concatenate all dwi inputs using mrcat.
+inputs=$(ls raw*.mif)
+
+mrcat $inputs dwi_out.mif
 
 # Inform user.
 echo "Converting output files to nifti format for storage..."
 
-# Convert mif back to nifti for storage.
+# Make an output directory.
 mkdir -p output
+
+# Convert mif back to nifti for storage.
 mrconvert dwi_out.mif output/dwi.nii.gz -export_grad_fsl output/dwi.bvecs output/dwi.bvals -export_grad_mrtrix dwi_out.b -json_export output/dwi.json -quiet -force
 
 # Remove temp files.
-rm raw1.b raw1.mif raw2.b raw2.mif dwi_out.b dwi_out.mif
+rm raw*.b raw*.mif dwi_out.b dwi_out.mif
 
 
